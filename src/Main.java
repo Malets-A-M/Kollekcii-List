6842bbe63b810da0019633b7b2783b16443ed284
Salam4ik <andrej160195@mail.ru>
2023-01-28 01:20:00 +0300

    Оптимизировал и исправил метод поиска

--- src/Main.java
+++ src/Main.java
@@ -20,55 +20,68 @@ public class Main {
                 int numberCommand = Integer.parseInt(numberCommandString);
                 switch (numberCommand) {
                     case 1 -> {
-                        System.out.println("Какую покупку хотите добавить?");
-                        market.add(scanner.nextLine());
-                        System.out.println("Итого в списке покупок: " + market.size());
+                        addProduct(market, scanner);
                     }
                     case 2 -> {
-                        System.out.println("Список покупок:");
-                        for (int a = 0; a < market.size(); a++) {
-                            System.out.println(a + 1 + ". " + market.get(a));
-                        }
+                        shoppingList(market);
                     }
                     case 3 -> {
-                        System.out.println("Список покупок:");
-                        for (int a = 0; a < market.size(); a++) {
-                            System.out.println(a + 1 + ". " + market.get(a));
-                        }
-                        System.out.println("Какую хотите удалить? Введите номер или название");
-                        String text = scanner.nextLine();
-                        try {
-                            int t = Integer.parseInt(text);
-                            System.out.println("Покупка \"" + market.get(t - 1) + "\" удалена, список покупок:");
-                            market.remove(t - 1);
-                            for (int a = 0; a < market.size(); a++) {
-                                System.out.println(a + 1 + ". " + market.get(a));
-                            }
-                        } catch (NumberFormatException exception) {
-                            market.remove(text);
-                            System.out.println("Покупка \"" + text + "\" удалена, список покупок:");
-                            for (int a = 0; a < market.size(); a++) {
-                                System.out.println(a + 1 + ". " + market.get(a));
-                            }
-                        }
+                        shoppingList(market);
+                        deleteProduct(market, scanner);
                     }
                     case 4 -> {
-                        System.out.println("Введите текст для поиска:");
-                        String text = scanner.nextLine();
-                        System.out.println("Найдено:");
-                        for (int a = 0; a < market.size(); a++) {
-                            String itemLower = text.toLowerCase();
-                            String queryLower = market.get(a).toLowerCase();
-                            if (itemLower.contains(queryLower)) {
-                                System.out.println(a + 1 + ". " + market.get(a));
-                                continue;
-                            }
-                        }
+                        searchProduct(market, scanner);
                     }
                 }
             } catch (NumberFormatException exception){
                 continue;
             }
+        }
+    }
+
+    private static void searchProduct(List<String> market, Scanner scanner) {
+        System.out.println("Введите текст для поиска:");
+        String text = scanner.nextLine();
+        System.out.println("Найдено:");
+        for (int a = 0; a < market.size(); a++) {
+            String itemLower = text.toLowerCase();
+            String queryLower = market.get(a).toLowerCase();
+            if (queryLower.contains(itemLower)) {
+                System.out.println(a + 1 + ". " + market.get(a));
+                continue;
+            }
+        }
+    }
+
+    private static void deleteProduct(List<String> market, Scanner scanner) {
+        System.out.println("Какую хотите удалить? Введите номер или название");
+        String text = scanner.nextLine();
+        try {
+            int t = Integer.parseInt(text);
+            System.out.println("Покупка \"" + market.get(t - 1) + "\" удалена, список покупок:");
+            market.remove(t - 1);
+            for (int a = 0; a < market.size(); a++) {
+                System.out.println(a + 1 + ". " + market.get(a));
+            }
+        } catch (NumberFormatException exception) {
+            market.remove(text);
+            System.out.println("Покупка \"" + text + "\" удалена, список покупок:");
+            for (int a = 0; a < market.size(); a++) {
+                System.out.println(a + 1 + ". " + market.get(a));
+            }
+        }
+    }
+
+    private static void addProduct(List<String> market, Scanner scanner) {
+        System.out.println("Какую покупку хотите добавить?");
+        market.add(scanner.nextLine());
+        System.out.println("Итого в списке покупок: " + market.size());
+    }
+
+    private static void shoppingList(List<String> market) {
+        System.out.println("Список покупок:");
+        for (int a = 0; a < market.size(); a++) {
+            System.out.println(a + 1 + ". " + market.get(a));
         }
     }
 }
